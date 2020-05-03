7592fb052994985c801e1be2309689f5f36a4541
lucasliet <lucasouliveira@gmail.com>
2020-05-03 01:55:41 -0300

    Finished app 🤑

---
+++ lib/story.dart
@@ -0,0 +1,17 @@
+class Story {
+  String _storyTitle;
+  String _choice1;
+  String _choice2;
+
+  Story({String storyTitle, String choice1, String choice2}){
+    this._storyTitle = storyTitle;
+    this._choice1 = choice1;
+    this._choice2 = choice2;
+  }
+
+  String get choice2 => _choice2;
+
+  String get choice1 => _choice1;
+
+  String get storyTitle => _storyTitle;
+}
